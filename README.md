8bcc7ba8cbc300867948a8f0a4b5b0970f19e6c1
aqua <84078890+phthallo@users.noreply.github.com>
2025-11-18 16:47:16 -0500

    feat: restructure dns interpretation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # fanfi.cx
 Query and read fanfiction from the Archive of Our Own over DNS. Inspired by [ch.at](https://github.com/Deep-ai-inc/ch.at). Runs on a single Go binary.
+
+
+## Usage
+```
+dig @fanfi.cx "your query here" -p 1337 +short
+```
+
+`+short` is optional but will make output look neater.
+
+### Query terms
+
+`[work_id] 22222` will fetch the work with the ID 22222. If used without `[chapter]`, it will fetch only the first chapter by default.
+`[chapter] 3` can be used in conjunction with `[work_id]` and can be used for chapter-ination (pagination?? but for chapters??)
+`[search] search query here` will search for that term. 
+
+If no parameters are specified, it will default to searching. for that term.
+
+For instance, `dig @fanfi.cx "[work_id] 17400464 [chapter] 3" -p 1337 +short` is a valid query. 
+`dig @fanfi.cx "[search] stag beetles and broken legs" -p 1337 +short` is also a valid query. 
--- internal/handler.go
+++ internal/handler.go
@@ -4,54 +4,102 @@ import (
 	"github.com/256dpi/newdns"
 	"fmt"
 	"regexp"
+	"strings"
+	"strconv"
 )
 
-func sanitiseDNSLabel(label string) string {
-	label = regexp.MustCompile(`[\\ ]`).ReplaceAllString(label, "+")
-	re := regexp.MustCompile(`[^a-zA-Z0-9+._~-]`)
-	return re.ReplaceAllString(label, "-")
+type Search struct {
+	WorkId string
+	SearchQuery string
+	Chapter int 
+}
+
+func validateAndSanitiseDNSLabel(label string) (map[string]string, error) {
+	label = strings.ReplaceAll(label, `\ `, ` `) // dns thingy will always escape characters
+	params := regexp.MustCompile(`\[(\w+)\]\s+([^\[]+)`).FindAllStringSubmatch(label, -1)	
+	fmt.Println(params)	
+
+	validParams := make(map[string]string)
+
+	for i, match := range params {
+		tag := match[1]
+		phrase := strings.TrimSpace(match[2])
+		
+		if (tag == "chapter"){
+			if i == 0 { 
+				continue 
+			}
+			prevTag := params[i-1][1] 
+			if (prevTag != "work_id" && prevTag != "search") {
+				continue 
+			}
+
+			if _, err := strconv.Atoi(phrase); err != nil {		
+				return nil, fmt.Errorf("Provided chapter is not a number!")
+			}	
+		}
+		
+		if tag == "search" {
+			if (i+1 < len(params) && params[i+1][1] == "chapter") {
+				continue 
+			} else {
+				// make url friendly
+				phrase = regexp.MustCompile(`[\\ ]`).ReplaceAllString(phrase, "+") 
+				phrase = regexp.MustCompile(`[^a-zA-Z0-9+._~-]`).ReplaceAllString(phrase, "-")
+			}
+		}
+		
+    validParams[tag] = phrase
+
+	}
+	fmt.Println(validParams)
+	return validParams, nil
 }
 
 func Handler(name string) ([]newdns.Set, error) {
-		var query []Work
-		var err error
+		var searchResults []Work
 		var fqdn = name + "fanfi.cx."
-		var newName = sanitiseDNSLabel(name)
+		var parsedSearchParams, err = validateAndSanitiseDNSLabel(name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		var searchQuery = parsedSearchParams["search"]
+		fmt.Println("Search query",  parsedSearchParams["search"], "Chapter",  parsedSearchParams["chapter"], "WorkId",  parsedSearchParams["work_id"])
+		// var output string
 
-		if newName != "" {
-			query, err = QuerySearchResults(newName)
+		sets := []newdns.Set{
+			{
+				Name: fqdn,
+				Type: newdns.A,
+				Records: []newdns.Record{
+					{Address: "1.2.3.4"},
+				},
+			},
+			{
+				Name: fqdn,
+				Type: newdns.AAAA,
+				Records: []newdns.Record{
+					{Address: "1:2:3:4::"},
+				},
+			},
+		}
+
+		txtSet := newdns.Set {
+				Name: fqdn,
+				Type: newdns.TXT,
+				Records: []newdns.Record{},
+		}
+
+		if searchQuery != "" {
+			searchResults, err = QuerySearchResults(searchQuery)
 			if err != nil {
 				fmt.Println("this is where the err", err)
 			}
 
-			sets := []newdns.Set{
-				{
-					Name: fqdn,
-					Type: newdns.A,
-					Records: []newdns.Record{
-						{Address: "1.2.3.4"},
-					},
-				},
-				{
-					Name: fqdn,
-					Type: newdns.AAAA,
-					Records: []newdns.Record{
-						{Address: "1:2:3:4::"},
-					},
-				},
-			}
-
-			txtSet := newdns.Set{
-				Name: fqdn,
-				Type: newdns.TXT,
-				Records: []newdns.Record{},
-			}
-			for _, txt := range FormatSearchResults(query) {
+			for _, txt := range FormatSearchResults(searchResults) {
 				txtSet.Records = append(txtSet.Records, newdns.Record{Data: []string{txt}})
 			}
-			sets = append(sets, txtSet)
-			return sets, nil
 		}
-
-			return nil, nil
+		sets = append(sets, txtSet)
+		return sets, nil
 		}
